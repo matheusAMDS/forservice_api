ace310b87d4eb5271c26eeaf28bcd3473f949dc1
matheusAMDS <matheusa56@gmail.com>
2020-10-06 18:00:12 -0300

    add heroku database

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,11 @@
 module.exports = {
   type: 'postgres',
-  host: process.env.DB_HOST,
+  url: process.env.DATABASE_URL,
+  /* host: process.env.DB_HOST,
   post: Number(process.env.PORT),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  database: process.env.DB_NAME, */
   synchronize: false,
   entities: ["src/app/models/**/*.ts"],
   migrations: ["src/database/migrations/**/*.ts"],
