bfe7f75d98e71b856e483dfe50e4b7a04adeb290
matheusAMDS <matheusa56@gmail.com>
2020-10-06 22:06:04 -0300

    aaaaa

--- ormconfig.js
+++ ormconfig.js
@@ -4,12 +4,12 @@ const ext = nodeEnv === 'production' ? 'js' : 'ts'
 
 module.exports = {
   type: 'postgres',
-  //url: process.env.DATABASE_URL,
-  host: process.env.DB_HOST,
-  post: Number(process.env.PORT),
+  url: process.env.DATABASE_URL,
+  /* host: process.env.DB_HOST,
+  post: Number(process.env.DB_PORT),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  database: process.env.DB_NAME, */
   synchronize: false,
   entities: [`${dir}/app/models/**/*.${ext}`],
   migrations: [`${dir}/database/migrations/**/*.${ext}`],
