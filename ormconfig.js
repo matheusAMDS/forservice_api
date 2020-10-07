6faebba1386365ce49bf8ebed662088abcf1aa4a
matheusAMDS <matheusa56@gmail.com>
2020-10-06 21:59:54 -0300

    fix in  uild process

--- ormconfig.js
+++ ormconfig.js
@@ -1,16 +1,20 @@
+const nodeEnv = process.env.NODE_ENV
+const dir = nodeEnv === 'production' ? 'build' : 'src'
+const ext = nodeEnv === 'production' ? 'js' : 'ts'
+
 module.exports = {
   type: 'postgres',
-  url: process.env.DATABASE_URL,
-  /* host: process.env.DB_HOST,
+  //url: process.env.DATABASE_URL,
+  host: process.env.DB_HOST,
   post: Number(process.env.PORT),
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME, */
+  database: process.env.DB_NAME,
   synchronize: false,
-  entities: ["src/app/models/**/*.ts"],
-  migrations: ["src/database/migrations/**/*.ts"],
+  entities: [`${dir}/app/models/**/*.${ext}`],
+  migrations: [`${dir}/database/migrations/**/*.${ext}`],
   cli: {
-    entitiesDir: "src/app/models",
-    migrationsDir: "src/database/migrations"
+    entitiesDir: `${dir}/app/models`,
+    migrationsDir: `${dir}/database/migrations`
   }
 }
